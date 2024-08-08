bfad03af0469ea9c6e999b437608898a1f88d94c
arastepa <stepanyan2298@gmail.com>
2024-08-08 17:25:48 +0400

    fix: make app responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,19 +4,34 @@ import Profile from '@/assets/profile.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
 import { setLogged } from '@/store/Loggedin/loggedSlice';
+import menu from '@/assets/menu.svg';
+import { useState } from 'react';
 
 const Header = () => {
   const logged = useSelector((state: RootState) => state.logged);
   const dispatch = useDispatch();
+  const [burgerClicked, setBurgerClicked] = useState(false);
   return (
     <ul className={styles.menu}>
-      <div>
-        <li>
-          <NavLink to={`/profile`}>
-            <img src={Profile} alt="" />
-          </NavLink>
-        </li>
-        <div className={styles.inner}>
+      <img
+        src={menu}
+        alt=""
+        className={`${styles.burger}`}
+        onClick={() => {
+          setBurgerClicked(burgerClicked === false ? true : false);
+        }}
+      />
+      <div className={styles.wrapper}>
+        <div className={styles.mobile}>
+          <li>
+            <NavLink to={`/profile`}>
+              <img src={Profile} alt="" />
+            </NavLink>
+          </li>
+        </div>
+        <div
+          className={`${styles.inner} ${burgerClicked ? styles.showInner : styles.hideInner} `}
+        >
           <li>
             <NavLink
               className={({ isActive }) =>
@@ -47,19 +62,49 @@ const Header = () => {
               About
             </NavLink>
           </li>
+          <div className={`${styles.loginItem} ${styles.loginMobile}`}>
+            {!logged.value && (
+              <li>
+                <NavLink
+                  className={({ isActive }) =>
+                    `${isActive ? `${styles.activeLogin}` : ``}`
+                  }
+                  to={`/login`}
+                >
+                  Login
+                </NavLink>
+              </li>
+            )}
+            {logged.value && (
+              <li>
+                <NavLink
+                  className={styles.logout}
+                  onClick={() => {
+                    localStorage.clear();
+                    dispatch(setLogged(false));
+                  }}
+                  to={`/login`}
+                >
+                  Logout
+                </NavLink>
+              </li>
+            )}
+          </div>
         </div>
       </div>
-      <div className={styles.loginItem}>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              `${isActive ? `${styles.activeLogin}` : ``}`
-            }
-            to={`/login`}
-          >
-            Login
-          </NavLink>
-        </li>
+      <div className={`${styles.loginItem} ${styles.loginDesktop}`}>
+        {!logged.value && (
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                `${isActive ? `${styles.activeLogin}` : ``}`
+              }
+              to={`/login`}
+            >
+              Login
+            </NavLink>
+          </li>
+        )}
         {logged.value && (
           <li>
             <NavLink
