7ac1be505f307215ab7b293e82066073c8160163
arastepa <stepanyan2298@gmail.com>
2024-08-08 16:10:44 +0400

    feat: about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,34 @@
+import styles from '@/pages/About/About.module.css';
+
 const About = () => {
-  return <div>Content</div>;
+  const data = {
+    name: 'Ara Stepanyan',
+    role: 'Frontend Developer',
+    location: 'Yerevan',
+    bio: 'I am a skilled developer capable of creating client-side websites. I am always eager to learn new technologies and languages. My technical stack includes C, C++, Docker-Compose, JavaScript, TypeScript, React, Redux toolkit, vite, webpack. I have completed the basic projects at 42 yerevan , and now student at rs school.',
+    contributions: 'frontend project',
+    github: 'https://github.com/arastepa',
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>{data.name}</h1>
+      <p>
+        <strong>role</strong> {data.role}
+      </p>
+      <p>
+        <strong>location</strong> {data.location}
+      </p>
+      <p>
+        <strong>about me </strong>
+        {data.bio}
+      </p>
+      <p>
+        <strong>contribution </strong>
+        {data.contributions}
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,5 +1,26 @@
+import styles from '@/pages/Contact/Contact.module.css';
+import time from '@/assets/time.svg';
+import location from '@/assets/location.svg';
+import phone from '@/assets/phone.svg';
+
 const Contact = () => {
-  return <div>Content</div>;
+  return (
+    <div className={styles.contact}>
+      <h1>Contact Us</h1>
+      <p>
+        <img src={location} alt="" />
+        <span>8558 Green Rd., LA</span>
+      </p>
+      <p>
+        <img src={phone} alt="" />
+        <span>+1 (603) 555-0123</span>
+      </p>
+      <p>
+        <img src={time} alt="" />
+        <span>Mon-Sat: 9:00 AM – 23:00 PM</span>
+      </p>
+    </div>
+  );
 };
 
 export default Contact;
