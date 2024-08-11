f0dcf693fc03b0ad7fe4a2afc6bfb84af8429ea2
arastepa <stepanyan2298@gmail.com>
2024-08-11 08:34:42 +0400

    fix: use flex instead of margin

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,19 +6,22 @@ import Contact from '@/pages/Contact/Contact';
 import About from '@/pages/About/About';
 import Login from '@/pages/Login/Login';
 import Profile from '@/pages/Profile/profile';
+import styles from '@/app.module.css';
 
 const AppRoutes = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <div className={styles.cont}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </div>
     </>
   );
 };
